ca9717fb163095da964248fed2e4df1b5034c2a2
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-15 00:15:50 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  
+  sq_arr = []
+  i = 0
+
+  while i < array.length do
+  
+  sq_arr << array[i] * array[i] 
+  
+  i += 1
+    
+  end
+
+  return sq_arr
+  
 end
